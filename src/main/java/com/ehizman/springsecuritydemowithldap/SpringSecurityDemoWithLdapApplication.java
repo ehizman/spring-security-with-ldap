fe6c9731c3de61fb864867d1a39d1d6787d6e449
Ehis <edemaehiz@gmail.com>
2021-12-19 01:51:48 +0100

    added dependencies- ldap core, ldap security, unboundid

---
+++ src/main/java/com/ehizman/springsecuritydemowithldap/SpringSecurityDemoWithLdapApplication.java
@@ -0,0 +1,13 @@
+package com.ehizman.springsecuritydemowithldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoWithLdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDemoWithLdapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ehizman/springsecuritydemowithldap/SpringSecurityDemoWithLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehizman.springsecuritydemowithldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoWithLdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
